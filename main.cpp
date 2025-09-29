3cc3e621b22094a189e2368b65b784b6cdf72199
tddgit096-pc <>
2025-09-28 19:53:21 -0700

    setters made

--- main.cpp
+++ main.cpp
@@ -8,11 +8,26 @@ class Color{
     int g;
     int b;
 
-    public:
+
     //constructor
     //destructor
     //setters
+    int valueClamp(int n){
+        if(n>255) //upper bound.
+            return 255;
+        if(r<0) //lowerbound
+            return 0;
+        return n;
+    }
+    public:
+    //setters
+    void setR(int r){this->r=valueClamp(r)};
+    void setG(int g){this->g=valueClamp(g)};
+    void setB(int b){this->b=valueClamp(b)};
     //getters
+    void getR(int r){};
+    void getG(int g){};
+    void getB(int b){};
 };
 
 int main(){
