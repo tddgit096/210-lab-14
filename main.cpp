f31108389d2532f2210e7b60daf0ddc403f33611
tddgit096-pc <>
2025-09-28 20:02:09 -0700

    create Color Objs: Populate list

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 //COMSC 210 | Lab 14| Toma Dimov
 #include <iostream>
+#include <iomanip>
+#include <vector>
 using namespace std;
 
 class Color{
-    //properties
+//properties
     int r;
     int g;
     int b;
 
-
-    //constructor
+//constructor
     Color(int r, int b, int g){setR(r);setB(b);setG(g);}
     //a function to clamp the incoming values to range 0-255
     int valueClamp(int n){
@@ -20,16 +21,38 @@ class Color{
         return n;
     }
     public:
-    //setters
+//setters
     void setR(int r){this->r=valueClamp(r)};
     void setG(int g){this->g=valueClamp(g)};
     void setB(int b){this->b=valueClamp(b)};
-    //getters
+//getters
     int getR(){return r;};
     int getG(){return g;};
     int getB(){return b;};
+//print self
+    void print(){
+        string bar = "-------";
+        int charWidth = 10;
+        cout<<setw(charWidth)<<"Color#"<<setw(charWidth)<<"R value"<<setw(charWidth)<<"G value"<<setw(charWidth)<<"B value"<<endl;
+        cout<<setw(charWidth)<<bar<<setw(charWidth)<<bar<<setw(charWidth)<<bar<<setw(charWidth)<<bar<<endl;
+        for(int i =0;i<v.size();i++){
+            cout<<setw(charWidth)<<i+1;
+            cout<<setw(charWidth)<<v[i].r;
+            cout<<setw(charWidth)<<v[i].g;
+            cout<<setw(charWidth)<<v[i].b;
+            cout<<endl;
+        }
+    }
 };
 
 int main(){
+    srand(time(nullptr));
+    int randSize = (rand()%6)+5; //rand num from 5-10
+    vector<Color> colors;
+    for(int i=0;i<randSize;i++){
+        colors.push_back(/**/);
+    }
+
+    //print here
     return 0;
-} // namespace std;
+}
