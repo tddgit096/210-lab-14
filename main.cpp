45b66d6e04cc8bc5fb3240c377ecfeaa5aa32d0e
tddgit096-pc <>
2025-09-28 19:43:57 -0700

    init commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//COMSC 210 | Lab 14| Toma Dimov
+#include <iostream>
+using namespace std;
+
+class Color{
+    //properties
+    int r;
+    int g;
+    int b;
+
+    public:
+    //constructor
+    //destructor
+    //setters
+    //getters
+};
+
+int main(){
+    return 0;
+} // namespace std;
